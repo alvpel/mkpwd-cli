2c9e1813b2fa50bfc6040e26518ece0626cc18a0
Alvaro <alvpel@gmail.com>
2024-11-07 22:47:42 +0100

    added args to main for testing

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -3,13 +3,13 @@ import { parseConfig } from "../config/parseArgs.ts";
 import { generatePassword } from "../password/generator.ts";
 import { showHelp } from "./help.ts";
 
-async function main() {
-    if (Deno.args.includes("--help")) {
+export async function main(args: string[] = Deno.args) {
+    if (args.includes("--help")) {
         showHelp();
         return;
     }
     try { 
-        const config = parseConfig(Deno.args);
+        const config = parseConfig(args);
         
         const password = generatePassword(config);
         await copyToClipboard(password);
