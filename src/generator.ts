b01493295bc4b3bcf373ddae2c44069e0ec15862
Alvaro <alvpel@gmail.com>
2024-11-07 21:43:41 +0100

    fixed bug on shuffle array

--- src/generator.ts
+++ src/generator.ts
@@ -35,7 +35,7 @@ function pickRandom(characters: string): string {
 
 function shuffleArray(array: string[]): string[] {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i *1));
+        const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
