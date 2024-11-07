a070fff3143768e2fd47778f47523a8d36a41412
Alvaro <alvpel@gmail.com>
2024-11-07 17:18:14 +0100

    Fixed config bugs

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,9 @@ export function getConfig(args: string[]) {
 
     // Set default values
     const length = parsedArgs.length ? Number(parsedArgs.length) : 12;
-    const useLetters = parsedArgs.letters !== undefined ? parsedArgs.letters : true;
-    const useNumbers = parsedArgs.numbers !== undefined ? parsedArgs.numbers : true;
-    const useSpecial = parsedArgs.special !== undefined ? parsedArgs.special : false;
+    const useLetters = parsedArgs.letters !== undefined ? parsedArgs.letters !== "false" : true;
+    const useNumbers = parsedArgs.numbers !== undefined ? parsedArgs.numbers !== "false" : true;
+    const useSpecial = parsedArgs.special !== undefined ? parsedArgs.special !== "false" : false;
 
     if (isNaN(length) || length <= 0) throw new Error("Invalid length");
     if (!useLetters && !useNumbers && !useSpecial) throw new Error("Select at least one character type");
