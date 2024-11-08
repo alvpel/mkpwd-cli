088eccfd5abea76e2c430febf1da405ada6ed8c3
Alvaro <alvpel@gmail.com>
2024-11-08 22:35:09 +0100

    made pickRadnom and shuffleArray generic functions

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -3,7 +3,7 @@ export function pickRandom<T>(items: T[] | string): T | string {
     return items[randomIndex];
 }
 
-export function shuffleArray(array: string[]): string[] {
+export function shuffleArray<T>(array: T[]): T[] {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
