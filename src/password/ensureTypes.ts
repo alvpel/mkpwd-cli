f18389f83c96205b64021cc4d3f0f5703e95245c
Alvaro <alvpel@gmail.com>
2024-11-08 22:36:35 +0100

    specify password type in ensureTypes

--- src/password/ensureTypes.ts
+++ src/password/ensureTypes.ts
@@ -3,7 +3,7 @@ import { pickRandom } from "../utils/random.ts";
 import { LETTERS, NUMBERS, SPECIAL } from "./charSet.ts";
 
 export function ensureCharacterTypes(config: PasswordConfig): string[] {
-    const password = [];
+    const password: string[] = [];
 
     if (config.useLetters) password.push(pickRandom(LETTERS + LETTERS.toLocaleUpperCase()));
     if (config.useNumbers) password.push(pickRandom(NUMBERS));
