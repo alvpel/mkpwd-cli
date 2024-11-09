4a22baf47dd18af63caee44114803d338d9bc98d
Alvaro <alvpel@gmail.com>
2024-11-09 11:27:10 +0100

    added wordlist, refactored passphrase to use generic functions

--- src/passphrase/generator.ts
+++ src/passphrase/generator.ts
@@ -1,18 +1,13 @@
+import { loadListFromFile } from "../utils/loadList.ts";
 import { pickRandom } from "../utils/random.ts";
+import type { PassphraseConfig } from "../config/types.ts";
 
 const WORD_LIST_PATH = './src/passphrase/wordlist.txt';
 
-// loadWordList
-async function loadWordList(): Promise<string[]> {
-    const text = await Deno.readTextFile(WORD_LIST_PATH);
-    return text.split("\n").map(word => word.trim()).filter(Boolean);
-}
-
-// generatePassphrase
-export async function generatePassphrase(length: number = 4, separator: string = "-"): Promise<string> {
-    const words = await loadWordList();
+export async function generatePassphrase(config: PassphraseConfig): Promise<string> {
+    const words = await loadListFromFile(WORD_LIST_PATH);
     if (words.length === 0) throw new Error("Word list is empty");
 
-    const passphraseWords = Array.from({ length: length }, () => pickRandom(words));
-    return passphraseWords.join(separator);
+    const passphraseWords = Array.from({ length: config.wordCount }, () => pickRandom(words));
+    return passphraseWords.join(config.separator);
 }
