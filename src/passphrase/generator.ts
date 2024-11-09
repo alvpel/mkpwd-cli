61ba4c9715c0b6820e3b6aec49bc25a94a00806e
Alvaro <alvpel@gmail.com>
2024-11-09 16:32:22 +0100

    passphrase now uses embedded data

--- src/passphrase/generator.ts
+++ src/passphrase/generator.ts
@@ -1,11 +1,10 @@
 import { loadListFromFile } from "../utils/loadList.ts";
 import { pickRandom } from "../utils/random.ts";
 import type { PassphraseConfig } from "../config/types.ts";
-
-const WORD_LIST_PATH = '../passphrase/wordlist.txt';
+import { WORD_LIST_DATA_URL } from "./wordSet.ts";
 
 export async function generatePassphrase(config: PassphraseConfig): Promise<string> {
-    const words = await loadListFromFile(WORD_LIST_PATH);
+    const words = await loadListFromFile(WORD_LIST_DATA_URL);
     if (words.length === 0) throw new Error("Word list is empty");
 
     const passphraseWords = Array.from({ length: config.wordCount }, () => pickRandom(words));
